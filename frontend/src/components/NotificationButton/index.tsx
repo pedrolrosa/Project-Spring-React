0497063090d7a2426f2894adec519e9875ca80fb
pedrolrosa <pedroluiz2109@gmail.com>
2022-11-20 17:19:07 -0300

    Send Notification

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,10 +1,23 @@
 import './style.css' 
+import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg'
+import { BASE_URL } from '../../utils/request';
 
-function NotificationButton() {
+type Props = {
+  saleId : number;
+}
+
+function handleClick(id : number){
+  axios(`${BASE_URL}/sales/${id}/notification`)
+  .then(response => {
+    console.log("Mensagem Enviada");
+  })
+}
+
+function NotificationButton( {saleId} : Props ) {
     return(
       <>
-        <div className='dsmeta-red-btn'>
+        <div className='dsmeta-red-btn' onClick={() => handleClick(saleId)}>
             <img src={icon} alt="View Notification" />
         </div>
       </>
